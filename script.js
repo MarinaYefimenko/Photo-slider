e130dcb06c919f8b96a2d51c7b99f04777e37fbe
MYefimenko <marinka0410@gmail.com>
2022-09-19 14:03:54 +0200

    withRegExp

--- script.js
+++ script.js
@@ -93,42 +93,45 @@ window.addEventListener('DOMContentLoaded', () => {
                 dots[slideIndex - 1].style.opacity = 1;
             }
 
+            function changeSlides () {
+                moveSlide ();
+                addZero ();
+                setActiveDot ();
+            }
+
+            function transfToDigit (str) {
+                return +str.replace(/\D/g, '');
+            }
+
 
             next.addEventListener('click', () => {
-                if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+                if (offset == transfToDigit(width) * (slides.length - 1)) {
                     offset = 0;
                 } else {
-                    offset += +width.slice(0, width.length - 2);
+                    offset += transfToDigit(width);
                 }
                 
-                moveSlide ();
-
                 if (slideIndex == slides.length) {
                     slideIndex = 1;
                 } else {
                     slideIndex++;
                 }
-
-                addZero ();
-                setActiveDot ();                
+                changeSlides ();               
             })
 
             prev.addEventListener('click', () => {
                 if (offset == 0) {
-                    offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+                    offset = transfToDigit(width) * (slides.length - 1);
                 } else {
-                    offset -= +width.slice(0, width.length - 2);
+                    offset -= transfToDigit(width);
                 }
-                moveSlide ();
-
+               
                 if (slideIndex == 1) {
                     slideIndex = slides.length;
                 } else {
                     slideIndex--;
                 }
-
-                addZero ();
-                setActiveDot ();
+                changeSlides (); 
             })
 
             dots.forEach(dot => {
@@ -136,12 +139,9 @@ window.addEventListener('DOMContentLoaded', () => {
                     const slideTo = e.target.getAttribute('data-slide-to');
         
                     slideIndex = slideTo;
-                    offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+                    offset = transfToDigit(width) * (slideTo - 1);
                     
-                    moveSlide ();
-                    
-                    addZero ();
-                    setActiveDot ();
+                    changeSlides (); 
                 });
             })
         })
